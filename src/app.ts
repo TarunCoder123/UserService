10aa0527767a2f631caa5701b9526e55706f8c00
tarun-pratap <7tarun7singh@gmail.com>
2025-07-20 17:55:02 +0530

    Second type of rate limit custom one

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
-import { Response,Request,NextFunction } from "express";
+import { Response, Request, NextFunction } from "express";
 import Controller from "./interfaces/controller.interfaces";
-import {env} from "./config/env";
+import { env } from "./config/env";
 import { log } from "./utils/helper.utils";
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
@@ -13,22 +13,22 @@ import { rateLimiter } from "./middleware/rateLimit.middleware";
 require('dotenv').config
 
 class App {
-    public app:express.Application;
+    public app: express.Application;
     // public req:express.Request;
     // public res:express.Response;
     // public next:express.NextFunction;
 
-    constructor(controllers: Controller[]){
-        this.app=express();
+    constructor(controllers: Controller[]) {
+        this.app = express();
         this.initializeMiddlewares();
         this.initializeControllers(controllers);
     }
     /**
      * Bind the port with the application on which this app is running
      */
-    public listen(){
-        this.app.listen(env.PORT?Number(env.PORT):7200,()=>{
-            log.blue(`app is listenging at ${env.PORT?Number(env.PORT):7200}`);
+    public listen() {
+        this.app.listen(env.PORT ? Number(env.PORT) : 7200, () => {
+            log.blue(`app is listenging at ${env.PORT ? Number(env.PORT) : 7200}`);
         });
     }
 
@@ -37,22 +37,22 @@ class App {
      * if you try to call without calling the listen then it return default null value as server
      * @return http server instance
      */
-    public getServer():express.Application {
+    public getServer(): express.Application {
         return this.app;
     }
     private initializeMiddlewares() {
         this.app.use(
             cors({
-              credentials: true,
-              origin: process.env.ORIGIN?.split(',') || undefined,
-              methods:["GET","POST","PUT","DELETE","OPTIONS","HEAD"],
-              preflightContinue: false
+                credentials: true,
+                origin: process.env.ORIGIN?.split(',') || undefined,
+                methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"],
+                preflightContinue: false
             })
         );
         this.app.use(express.json());
-        this.app.use(express.urlencoded({extended: false}));
+        this.app.use(express.urlencoded({ extended: false }));
         this.app.use(cookieParser());
-        this.app.use(function onError(err:any, req:Request, res: any, next:NextFunction) {
+        this.app.use(function onError(err: any, req: Request, res: any, next: NextFunction) {
             // The error id is attached to `res.sentry` to be returned
             // and optionally displayed to the user for support.
             res.statusCode = 500;
@@ -60,24 +60,26 @@ class App {
             next();
         });
         this.app.use(rateLimiter);
+        // Apply globally: max 5 requests per 10 seconds
+        // this.app.use(customRateLimiter({ windowMs: 10 * 1000, maxRequests: 5 }));
     }
 
-    private initializeControllers(controllers:Controller[]) {
+    private initializeControllers(controllers: Controller[]) {
         // Check api status
-        this.app.get("/",(req:Request,res:Response)=>{
-           sendResponse(res,{message: RESPONSE_MESSAGES.API_SERVICE})
+        this.app.get("/", (req: Request, res: Response) => {
+            sendResponse(res, { message: RESPONSE_MESSAGES.API_SERVICE })
         });
         // Setup the controllers
-        controllers.forEach((controller)=>{
-           this.app.use('/api',controller.router);
+        controllers.forEach((controller) => {
+            this.app.use('/api', controller.router);
         });
-             //Unknown rotues handler
-    //  this.app.all("*",(req:Request,res: Response)=>{
-    //     sendResponse(res,{
-    //         status: STATUS_CODES.UNAUTHORIZED,
-    //         message:RESPONSE_MESSAGES.ROUTE_404,
-    //     })
-    //  });
+        //Unknown rotues handler
+        //  this.app.all("*",(req:Request,res: Response)=>{
+        //     sendResponse(res,{
+        //         status: STATUS_CODES.UNAUTHORIZED,
+        //         message:RESPONSE_MESSAGES.ROUTE_404,
+        //     })
+        //  });
     }
 }
 
--- src/middleware/rateLimit.middleware.ts
+++ src/middleware/rateLimit.middleware.ts
@@ -10,3 +10,40 @@ export const rateLimiter = rateLimit({
     error: 'Too many requests, please try again later.',
   },
 });
+
+
+// This is the custom rate limit which I can use
+// // Store request timestamps per user (IP in this case)
+// const rateLimitStore = new Map();
+
+// function customRateLimiter({ windowMs, maxRequests }) {
+//   return (req, res, next) => {
+//     const userKey = req.ip; // You can change this to req.headers['x-api-key'] or token
+//     const currentTime = Date.now();
+
+//     if (!rateLimitStore.has(userKey)) {
+//       rateLimitStore.set(userKey, []);
+//     }
+
+//     // Get timestamps for this user
+//     const timestamps = rateLimitStore.get(userKey);
+
+//     // Filter out timestamps that are older than the window
+//     const updatedTimestamps = timestamps.filter(ts => currentTime - ts < windowMs);
+
+//     // If under limit, allow request and store timestamp
+//     if (updatedTimestamps.length < maxRequests) {
+//       updatedTimestamps.push(currentTime);
+//       rateLimitStore.set(userKey, updatedTimestamps);
+//       next();
+//     } else {
+//       // Too many requests
+//       res.status(429).json({
+//         message: `Rate limit exceeded. Try again later.`,
+//       });
+//     }
+//   };
+// }
+
+// module.exports = customRateLimiter;
+
